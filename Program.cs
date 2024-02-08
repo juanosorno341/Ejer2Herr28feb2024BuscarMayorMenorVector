4253c88a6823cdefa29538ec1f60e811b561271f
juan carlos osorno <juan.osorno341@pascualbravo.edu.co>
2024-02-08 13:57:24 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Ejer2Herr28feb2024BuscarMayorMenorVector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese el indice");
+            int indice = int.Parse(Console.ReadLine());
+            int[] dat = new int[indice];
+            for (int i = 0; i < dat.Length; i++)
+            {
+                Console.WriteLine("Entra el dato [" + i + "]");
+                dat[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < dat.Length; i++)
+            {
+                Console.WriteLine("\n" + dat[i]);
+            }
+
+            Console.WriteLine($"\nEl dato mayor es: {dat.Max()}");
+            Console.WriteLine($"El dato menor es: {dat.Min()}");
+
+
+            Console.ReadLine();
+            Console.ReadKey();
+
+        }
+    }
+}
